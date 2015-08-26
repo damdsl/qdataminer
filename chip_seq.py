02903032ec19a0676a49eefc47aefd481f020529
Dos Santos Luis <damien.dos.santos.luis@gmail.com>
2015-08-26 14:14:29 +0200

    Update chip_seq.py

--- chip_seq.py
+++ chip_seq.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import json
 
+PeakDNAseq = read_csv(Peak_DNA_seq.csv, sep=';')
 df=pd.read_csv("df.csv",  sep=',')
+df = pd.merge(df, PeakDNAseq)
+
 df=df.rename(columns={'X.10.log10.pvalue.': 'X_10_log10_pvalue'})
 df=df.rename(columns={'FDR...': 'FDR'})
 df=df.drop('V4', 1)
 df=df.drop('V5', 1)
 df=df.drop('seqnames', 1)
 df=df.drop('Unnamed: 0', 1)
-df = pd.merge(df, PeakDNAseq)
+
 df=df.rename(columns={'x': 'DNA_sequence'})
 df = df.set_index('SYMBOL')
+df['annotation'] = tuple(x[0] for x in df['annotation'])
 
 with open('tmp.json', 'w') as f:
     f.write(df.to_json(orient='records'))
